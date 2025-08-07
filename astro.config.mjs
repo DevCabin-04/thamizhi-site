a8ebfafc0096056580567bbac027c5c5b2344ade
V.Lajeethan <109454694+lagithan@users.noreply.github.com>
2025-08-07 15:53:31 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ const isDev = process.env.NODE_ENV !== 'production';
 
 export default defineConfig({
   site: isDev ? 'http://localhost:4321' : 'https://devcabin-04.github.io',
+  base: isDev ? undefined : '/thamizhi-site',
   output: 'static',
   i18n: {
     defaultLocale: "en",
