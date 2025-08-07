268dfe579216c835ba291c18855c355fa2515ab6
lagithan <vimalrajlagithan@gmail.com>
2025-08-07 13:59:32 +0530

    tina integrated

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '21e25873934e8741e0ca8a009bd0c4709f87c98a', queries,  });
 export default client;
   
