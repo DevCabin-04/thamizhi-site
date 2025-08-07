79c05b94e1ffd8170e8fc5d7cc254076ff29672b
lagithan <vimalrajlagithan@gmail.com>
2025-08-07 13:29:16 +0530

    integrate with githubpages

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'C:/Users/lagit/Desktop/tamizhhe-pudated/thamizhi-site/tina/__generated__/.cache/1754553354186', url: 'https://content.tinajs.io/1.5/content/87932b95-cd47-4185-918e-568e132681aa/github/main', token: '21e25873934e8741e0ca8a009bd0c4709f87c98a', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/lagit/Desktop/tamizhhe-pudated/thamizhi-site/tina/__generated__/.cache/1754553435246', url: 'https://content.tinajs.io/1.5/content/87932b95-cd47-4185-918e-568e132681aa/github/main', token: '21e25873934e8741e0ca8a009bd0c4709f87c98a', queries,  });
 export default client;
   
